4875df54f73e87a2c674e08788b50c113956d656
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-02-26 23:20:34 +0000

    Connection to the RSS feed is now possible.

--- app/src/main/java/com/silviaodwyer/rssfeedreader/MainActivity.kt
+++ app/src/main/java/com/silviaodwyer/rssfeedreader/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "Main Activity"
@@ -18,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>(){
+        private class DownloadData : AsyncTask<String, Void, String>() {
             private val TAG = "Download Data"
 
             override fun onPostExecute(result: String?) {
@@ -26,12 +32,54 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg p0: String?): String {
+            override fun doInBackground(vararg URL: String?): String {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                Log.d(TAG, "doInBackground: starts with ${p0[0]}")
-                return "doInBackground completed"
+                Log.d(TAG, "doInBackground: starts with ${URL[0]}")
+                // If more than one URL is passed into the method, then
+                // those extra URLS can be accessed via URL[1], URL[2], etc.,
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    // Log.e refers to error messages
+                    Log.e(TAG, "doInBackground: Error downloading. :(")
+                }
+                return rssFeed
             }
         }
+
+        }
+
+        private fun downloadXML(urlPath: String?): String {
+            val xmlResult = StringBuilder()
+
+            try{
+                val url = URL(urlPath)
+                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                val response = connection.responseCode
+                Log.d(TAG, "downloadXML: The response code was $response")
+
+//                val inputStream = connection.inputStream
+//                val inputStreamReader = InputStreamReader(inputStream)
+//                val reader = BufferedReader(inputStreamReader)
+                val reader = BufferedReader(InputStreamReader(connection.inputStream))
+
+                val inputBuffer = CharArray(500)
+                var charsRead = 0
+                while(charsRead >= 0){
+                    charsRead = reader.read(inputBuffer)
+                    if (charsRead > 0){
+                        xmlResult.append(String(inputBuffer, 0, charsRead))
+                    }
+                }
+            } catch(e: MalformedURLException){
+                Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+            } catch (e: IOException){
+                Log.e(TAG, "downloadXML: IOException reading data: ${e.message}")
+            }
+
+            catch(e: Exception){
+                Log.e(TAG, "Unknown error: ${e.message}")
+            }
+
 
     }
 

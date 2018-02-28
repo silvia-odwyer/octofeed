420bf67e02ef61bb3d3748364af344dc241af6d7
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-02-28 22:29:53 +0000

    Thanks to Kotlin's readText() method, I condensed over thirty lines of code down to just one. Kotlin's one language, I tell you.

--- app/src/main/java/com/silviaodwyer/rssfeedreader/MainActivity.kt
+++ app/src/main/java/com/silviaodwyer/rssfeedreader/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.silviaodwyer.rssfeedreader
-
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import java.io.BufferedReader
 import java.io.IOException
@@ -12,75 +11,97 @@ import java.net.MalformedURLException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "Main Activity"
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "oncCreate called")
+        Log.d(TAG, "onCreate called")
         val downloadData = DownloadData()
-        downloadData.execute("URL goes here")
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
 
     companion object {
         private class DownloadData : AsyncTask<String, Void, String>() {
-            private val TAG = "Download Data"
+            private val TAG = "DownloadData"
 
             override fun onPostExecute(result: String?) {
                 super.onPostExecute(result)
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg URL: String?): String {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                Log.d(TAG, "doInBackground: starts with ${URL[0]}")
-                // If more than one URL is passed into the method, then
-                // those extra URLS can be accessed via URL[1], URL[2], etc.,
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
                 if (rssFeed.isEmpty()) {
-                    // Log.e refers to error messages
-                    Log.e(TAG, "doInBackground: Error downloading. :(")
+                    Log.e(TAG, "doInBackground: Error downloading")
                 }
                 return rssFeed
             }
-        }
 
-        }
-
-        private fun downloadXML(urlPath: String?): String {
-            val xmlResult = StringBuilder()
-
-            try{
-                val url = URL(urlPath)
-                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                val response = connection.responseCode
-                Log.d(TAG, "downloadXML: The response code was $response")
-
-//                val inputStream = connection.inputStream
-//                val inputStreamReader = InputStreamReader(inputStream)
-//                val reader = BufferedReader(inputStreamReader)
-                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-
-                val inputBuffer = CharArray(500)
-                var charsRead = 0
-                while(charsRead >= 0){
-                    charsRead = reader.read(inputBuffer)
-                    if (charsRead > 0){
-                        xmlResult.append(String(inputBuffer, 0, charsRead))
-                    }
-                }
-            } catch(e: MalformedURLException){
-                Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
-            } catch (e: IOException){
-                Log.e(TAG, "downloadXML: IOException reading data: ${e.message}")
+            private fun downloadXML(urlPath: String?): String {
+                // A very Kotlin like way of writing the below 30 lines of code!
+                // From thirty lines down to one! x) Amazing stuff
+                return URL(urlPath).readText()
             }
+//                val xmlResult = StringBuilder()
+//
+//                try {
+//                    val url = URL(urlPath)
+//                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+//                    val response = connection.responseCode
+//                    Log.d(TAG, "downloadXML: The response code was $response")
+//
+////
+////                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+////
+////                    val inputBuffer = CharArray(500)
+////                    var charsRead = 0
+////                    while (charsRead >= 0) {
+////                        charsRead = reader.read(inputBuffer)
+////                        if (charsRead > 0) {
+////                            xmlResult.append(String(inputBuffer, 0, charsRead))
+////                        }
+////                    }
+////                    reader.close()
 
-            catch(e: Exception){
-                Log.e(TAG, "Unknown error: ${e.message}")
-            }
+//                    Alternative One-Liner:
+//                    connection.inputStream.buffered().reader().use{ xmlResult.append(it.readText()) }
+//
+//                    Log.d(TAG, "Received ${xmlResult.length} bytes")
+//                    return xmlResult.toString()
 
-
+//                } catch (e: MalformedURLException) {
+//                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+//
+//
+//                } catch(e: SecurityException){
+//                    Log.e(TAG, "downloadXML: Security Exception. No internet permissions have been set! :(")
+//                }
+//                catch (e: IOException) {
+//                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+////                } catch (e: Exception) {
+////                    Log.e(TAG, "Unknown error: ${e.message}")
+//                }
+//                } catch (e: Exception) {
+//                    val errorMessage: String
+//                    when (e) {
+//                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
+//                        is IOException -> "downloadXML: IO Exception reading data ${e.message}"
+//                        is SecurityException -> {
+//                            e.printStackTrace()
+//                            "downloadXML: Security Exception: Needs permission? ${e.message}"
+//                        }
+//                        else -> "Unknown error: ${e.message}"
+//                    }
+//                }
+//
+//                return ""  // If it gets to here, there's been a problem. Return an empty string
+//            }
+//        }
+//    }
+        }
     }
-
 }
